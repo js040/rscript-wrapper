ac0b446e87dc205df57984a0ae8f1ff5d4c6c1cd
seano30 <jungbluth.sean@gmail.com>
2018-01-06 07:58:36 -0800

    fixes-after-ive-had-some-coffee-also-added-more-comments

--- fasta_headers_swap.py
+++ fasta_headers_swap.py
@@ -7,14 +7,18 @@ __maintainer__ = "Sean P. Jungbluth"
 __email__ = "jungbluth.sean@gmail.com"
 
 
+##import arguements
 #import os, sys, argparse, shutil, subprocess, time
 import os, sys, argparse, time
 from argparse import RawTextHelpFormatter
 from os.path import join
-from libs.logger import Logger
 
+##custom arguements (i.e. based on this python package only)
+from libs.logger import Logger
 from libs.versioncontrol import *
 
+
+##check if python3 being used (required for running). Not needed if shebang line points to "python3" and not "python"
 try:
     if sys.version_info.major != 3:
         sys.stderr.write('\nError: python version 3 is required - you have python version %d.\n\n' % sys.version_info.major)
@@ -24,25 +28,25 @@ except Exception:
     
 
 
-
+##function containing wrapper for R script 
 def run_fasta_headers_swap(shortnamefasta,keeptaxonomylookup,newlongnamefastafile,outputdir,filenameprefix,pathtoscripts):
     if not os.path.exists("output-directory"):
         os.makedirs("output-directory")
-    logfile = open(str(filenameprefix)+"_program.log", 'w')
-    logfile.write("***Start fasta_headers_swap***\n")
-    fastaheaderstime = time.time()
-    command=str(pathtoscripts)+'/libs/fasta_headers_swap.r '+str(shortnamefasta)+' '+str(keeptaxonomylookup)+' '+str(newlongnamefastafile)
+    logfile = open(str(filenameprefix)+"_program.log", 'w') #open log file, which logs all stdout
+    logfile.write("***Start fasta_headers_swap***\n") 
+    fastaheaderstime = time.time() 
+    command=str(pathtoscripts)+'/libs/fasta_headers_swap.r '+str(shortnamefasta)+' '+str(keeptaxonomylookup)+' '+str(newlongnamefastafile) #define command
     print("Full command: \n\n    "+str(command)+"\n")
-    process=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    for line in process.stdout:
+    process=subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #run command, and catch stdout and sterr
+    for line in process.stdout: #log all stout while running software
         sys.stdout.write(str(line))
         logfile.write(str(line)[2:-3])
         logfile.write('\n')
     process.wait()
     print("\n\nfasta_headers_swap ran in %s seconds" % round((time.time() - fastaheaderstime),2))
     logfile.write("***End fasta_headers_swap***\n")
-    out, err = process.communicate()
-    if process.returncode != 0: sys.exit("*Error running fasta_headers_swap*")
+    out, err = process.communicate() #pass stdout and stderr to python
+    if process.returncode != 0: sys.exit("*Error running fasta_headers_swap*") # if error code found during program, indicate it here
     else:
         logfile.close()
 #    for f in os.listdir(workingdirectory):
@@ -62,13 +66,13 @@ Wrapper for fasta_headers_swap.r version %s
     parser.add_argument("--keeptaxonomylookup", dest="keeptaxonomylookup", default='', help="""Description of keeptaxonomylookup""") #took a guess here that this is a y/n parameter
     parser.add_argument("--newlongnamefastafile", dest="newlongnamefastafile", default='', help="""Description of newlongnamefastafile""")
     parser.add_argument("--outputdir", dest="outputdir", default=os.getcwd(), help="""Indicate output directory (default: current working directory)""")
-    parser.add_argument("--filenameprefix", dest="filenameprefix", default="fasta-rename_", help="""Indicate output directory (default: current working directory""")
+    parser.add_argument("--filenameprefix", dest="filenameprefix", default="fasta-rename", help="""Indicate filename prefix (default: current working directory""")
     parser.add_argument("--pathtoscripts", dest="pathtoscripts", default='/Applications/ResearchSoftware/rscript-wrapper', help="""Indicate path to scripts (default: /Applications/ResearchSoftware/rscript-wrapper""")
     parser.add_argument('--version', action='version', version='%(prog)s v1.0')
     args = parser.parse_args()
-    if len(sys.argv) is None:
+    if len(sys.argv) is None: #print help if no flags declared
         parser.print_help()
-    elif len(sys.argv) < 3:
+    elif len(sys.argv) < 3: #print help if not all flags declared
         print('Need more arguements')
         parser.print_help()
     else:
@@ -96,6 +100,8 @@ Wrapper for fasta_headers_swap.r version %s
         print("    %s" % (python_version_found))
         print("    Rscript: v%s" % (rscript_version_found))  
         print("")
+
+# This commented out stuff is to clean up folders/files before running a script        
 #        my_file=Path(workingdirectory+'/'+args.filenameprefix+'_bioelement-table.txt')
 #        if my_file.exists():
 #            print('Warning: '+args.filenameprefix+'_bioelement-table.txt file already exists! - removing and regenerating')
@@ -106,7 +112,8 @@ Wrapper for fasta_headers_swap.r version %s
 
 
 
-        run_fasta_headers_swap(args.shortnamefasta,args.keeptaxonomylookup,args.newlongnamefastafile,args.outputdir,args.filenameprefix,args.pathtoscripts)
+        run_fasta_headers_swap(args.shortnamefasta,args.keeptaxonomylookup,args.newlongnamefastafile,args.outputdir,args.filenameprefix,args.pathtoscripts) #run program
+
 
 
         print('''
@@ -115,6 +122,8 @@ Wrapper for fasta_headers_swap.r version %s
 ***************************************************************************
         ''')
         print("fasta_header ran in %s seconds" % round((time.time() - starttime),2))
+ 
+# This commented out stuff is to clean up folders/files after running a script        
  #       for f in os.listdir(workingdirectory):
  #           if os.path.isdir(f) is True and "hmm-profiler-output" not in f:
  #               shutil.move(f, os.path.join(str(workingdirectory)+"/"+args.outputdir))
--- libs/logger.py
+++ libs/logger.py
@@ -9,7 +9,7 @@ class Logger(object):
         self.filename = filename
         out = filename.rsplit(".",1)[0]
         self.terminal = sys.stdout
-        self.log = open(str(out)+"-MAGi-log.txt", "a")
+        self.log = open(str(out)+"fasta-rename-log.txt", "a")
     def write(self, message):
         self.terminal.write(message)
         self.log.write(message)  
